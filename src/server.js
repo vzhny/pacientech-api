635a9ff3cad2f2fe0083a4635a74f23788ab7384
Diego Vizhnay <diego@vzhny.com>
2019-02-03 00:03:08 -0500

    Added basic logging to the express app | 02/03

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import timestamp from 'time-stamp';
 import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -8,6 +10,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// * Express Middleware
+app.use(
+  morgan(`${timestamp('MM/DD/YYYY')} :method :url :status :res[content-length] - :response-time ms`)
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
