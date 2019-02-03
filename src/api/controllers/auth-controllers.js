f8dca6da22f92517be78d2ce914638751479eea2
Diego Vizhnay <diego@vzhny.com>
2019-02-03 17:06:54 -0500

    Added the login user POST route | 02/03

--- src/api/controllers/auth-controllers.js
+++ src/api/controllers/auth-controllers.js
@@ -35,3 +35,36 @@ export const register = (req, res) => {
     }
   );
 };
+
+// POST login route controller
+export const login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).exec((error, user) => {
+    if (error) {
+      return res.status(400).json({
+        message: 'There was an error logging in. Please try again.',
+      });
+    }
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Could not find user or wrong password. Please try again.',
+      });
+    }
+
+    if (bcrypt.compareSync(password, user.password)) {
+      // eslint-disable-next-line
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: 86400 });
+
+      return res.status(200).json({
+        auth: true,
+        token,
+      });
+    }
+
+    return res.status(401).json({
+      message: 'Could not find user or wrong password. Please try again.',
+    });
+  });
+};
--- src/api/routes.js
+++ src/api/routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { register } from './controllers/auth-controllers';
+import { register, login } from './controllers/auth-controllers';
 
 const router = express.Router();
 
 // POST register route
 router.route('/auth/register').post(register);
+
+// POST login route
+router.route('/auth/login').post(login);
 
 export default router;
