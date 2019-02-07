f3b399154078f6666a6f9540aa0c75ad9b18334d
Diego Vizhnay <diego@vzhny.com>
2019-02-06 22:27:13 -0500

    Switched over to mlabs -- mongo atlas kept having connection issues | 02/06

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import {
-  MONGODB_URI_DEV,
-  MONGODB_URI_TEST,
+  MLABS_MONGODB_URI_DEV,
+  MLABS_MONGODB_URI_TEST,
   JWT_SECRET_DEV,
   JWT_SECRET_TEST,
   MONGODB_DB_NAME_DEV,
@@ -10,11 +10,11 @@ import {
 const env = process.env.NODE_ENV;
 
 if (env === 'development') {
-  process.env.MONGODB_URI = MONGODB_URI_DEV;
+  process.env.MONGODB_URI = MLABS_MONGODB_URI_DEV;
   process.env.JWT_SECRET = JWT_SECRET_DEV;
   process.env.MONGODB_DB_NAME = MONGODB_DB_NAME_DEV;
 } else if (env === 'test') {
-  process.env.MONGODB_URI = MONGODB_URI_TEST;
+  process.env.MONGODB_URI = MLABS_MONGODB_URI_TEST;
   process.env.JWT_SECRET = JWT_SECRET_TEST;
   process.env.MONGODB_DB_NAME = MONGODB_DB_NAME_TEST;
 }
