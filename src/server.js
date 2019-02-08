52c4d7443ff621e2f3e93ec0babfad9c53b147ca
Diego Vizhnay <diego@vzhny.com>
2019-02-07 19:26:10 -0500

    Separated app and server logic for more streamlined testing | 02/07

--- src/server.js
+++ src/server.js
@@ -1,41 +1,11 @@
-import './config/config';
-import dotenv from 'dotenv';
-import path from 'path';
-import express from 'express';
-import morgan from 'morgan';
-import timestamp from 'time-stamp';
-import helmet from 'helmet';
-import cors from 'cors';
-import database from './db/database'; // eslint-disable-line
-import routes from './api/routes';
+import app from './app';
 
-dotenv.config({ path: path.join(__dirname, '.env') });
-
-const app = express();
 const port = process.env.PORT || 3000;
 
-// * Express Middleware
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan(`${timestamp('MM/DD/YYYY')} :method :url :status STATUS - :response-time ms`));
-}
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(helmet());
-app.use(cors());
-
-// * API routes
-app.use('/api', routes);
-
-// * (Temporary) Heroku Wakeup Route
-app.get('/', (req, res) => {
-  const message = 'Please go to https://github.com/vzhny/pacientech-api for API usage information.';
-  res.status(200).send(message);
-});
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   if (process.env.NODE_ENV !== 'test') {
     console.log(`Server listening on port ${port}!`);
   }
 });
 
-export default app;
+export default server;
